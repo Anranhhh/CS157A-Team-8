0bfea2dadb6f2f3a06760f96e539c65c28ea2c13
giovannihsieh <giovannihsieh@gmail.com>
2025-07-24 22:34:00 -0700

    Update MySqlJdbcCreateTable.java
    added email for users table

--- src/main/java/MySqlJdbcCreateTable.java
+++ src/main/java/MySqlJdbcCreateTable.java
@@ -15,6 +15,7 @@ public class MySqlJdbcCreateTable {
                 "user_id INT NOT NULL AUTO_INCREMENT, " +
                 "username VARCHAR(50) NOT NULL, " +
                 "password VARCHAR(100) NOT NULL, " +
+		"email VARCHAR(100) NOT NULL, " +
                 "PRIMARY KEY (user_id))");
 
         // recipes table
